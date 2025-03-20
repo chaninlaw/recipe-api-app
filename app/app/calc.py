4f0fe1e33dfd4e6cd16c2cef4b803b0187a46744
ChaninL <chaninlawlert@gmail.com>
2025-03-20 22:45:53 +0700

    Update Dockerfile for PostgreSQL support, and add core app structure with database management command

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,19 +2,16 @@
 Calculator functions
 """
 
+
 def add(x: int, y: int):
-  """Add two numbers together."""
-  if (type(x) != int) or (type(y) != int):
-    print("Warning: Both arguments must be integers.")
-    return 0
-  
-  return x + y
+    """Add two numbers together."""
+    if (x is not int) or (y is not int):
+        return 0
+    return x + y
 
 
 def subtract(x: int, y: int,):
-  """Subtract two numbers together."""
-  if (type(x) != int) or (type(y) != int):
-    print("Warning: Both arguments must be integers.")
-    return 0
-  
-  return x - y
+    """Subtract two numbers together."""
+    if (x is not int) or (y is not int):
+        return 0
+    return x - y
